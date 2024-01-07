100bcb323d9648e21840a01427169cb7f6bb59d9
Alex Tacescu <alextac98@gmail.com>
2024-01-07 11:28:05 -0800

    Add a powershell script to create a symlink on Windows

--- WindowsSetup.ps1
+++ WindowsSetup.ps1
@@ -1,2 +1,20 @@
-# This script is not yet implemented
-Write-Error "This script is not yet developed or implemented."
+
+# We first need to check if we are running in an administrator console
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Host "You are not currently running in an administrator shell. Exiting the script."
+    Start-Process powershell.exe "-File",($MyInvocation.MyCommand.Definition) -Verb RunAs
+    exit
+}
+
+# Function to symlink the configs
+function Create-Symlink {
+    param (
+        [string]$originalFolder,
+        [string]$symlinkFolder
+    )
+    New-Item -ItemType SymbolicLink -Path $symlinkFolder -Target $originalFolder
+}
+
+# OrcaSlicer Configs
+Create-Symlink -symlinkFolder "$home\AppData\Roaming\OrcaSlicer" -originalFolder ".\OrcaSlicer"
